b6e8d80faac175e596d4d97fe26fd5d35b72de3d
Tony <a.s.savostyanov@gmail.com>
2026-01-18 13:39:04 +0100

    try

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
+import { copyFileSync } from "fs";
+import { join } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
