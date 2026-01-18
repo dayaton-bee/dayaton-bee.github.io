11287be7960eed43f5bb9ae0c3fdac020e78a271
Tony <a.s.savostyanov@gmail.com>
2026-01-18 13:36:28 +0100

    tmp

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,13 @@ import preact from "@preact/preset-vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [preact(),
+     {
+      name: 'copy-404',
+      closeBundle() {
+        const distPath = join(process.cwd(), 'dist');
+        copyFileSync(join(distPath, 'index.html'), join(distPath, '404.html'));
+      },
+    },],
   base: '/',
 });
